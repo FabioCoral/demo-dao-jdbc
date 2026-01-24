71543f7cbd171c21a65068108656d03b35a1bc74
Fabio Coral <fdcoral@hotmail.com>
2026-01-24 12:54:37 -0300

    Delete department implementation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -17,18 +17,26 @@ public class ProgramDepartment {
             System.out.println(department);
         }
 
+        Department department = new Department(null, "RH");
+
         System.out.println("\n=== Find department by id ===");
         System.out.println(departmentDao.findById(2));
-
+        /*
         System.out.println("\n=== Insert department ===");
-        Department department = new Department(null, "RH");
         departmentDao.insert(department);
         System.out.println("Department inserted !");
 
-        System.out.println("\n=== Insert department ===");
+
+        System.out.println("\n=== Update department ===");
         department.setName("HR");
         departmentDao.update(department);
         System.out.println("Department updated !");
+         */
+
+        System.out.println("\n=== Insert department ===");
+        departmentDao.deleteById(8);
+        System.out.println("Department deleted !");
+
 
         sc.close();
     }
